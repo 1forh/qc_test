de7ef83874f08d409979b86ede1e0e085fc4879e
Zachary Minner <1forhlol@gmail.com>
2015-10-13 20:59:56 -0400

    Added js files

---
+++ app/routes.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+// route - index
+router.get('/', function(request, response){
+	response.render('home');
+});
+
+// thank-you
+router.get('/results', function(request, response){
+	response.render('results');
+});
+
+// scraper
+router.get('/scrape', function(request, response){
+
+});
+
+// // route - about
+// app.get('/about', function(request, response){
+// 	response.render('about', { 
+// 		fortune: fortune.getFortune(), 
+// 		pageTestScript: '/tests/about.js' 
+// 	});
+// });
+
+// custom 404 page
+router.use(function(request, response){
+	response.status(404);
+	response.render('404');
+});
+// custom 500 page
+router.use(function(error, request, response, next){
+	console.error(error.stack);
+	response.status(500);
+	response.render('500');
+});
+
+
+module.exports = router;
