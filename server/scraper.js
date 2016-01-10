ddbe9dd6fca5c16facd67558370d1b5bb6b8ea64
Zachary Minner <1forhlol@gmail.com>
2016-01-09 21:00:43 -0500

    Refactor filter function for scraper

--- server/scraper.js
+++ server/scraper.js
@@ -21,18 +21,50 @@ module.exports = function(app){
     var domain = request.body.domain;
     console.log("Domain: " + domain);
 
-    // Save to json api
-      
+    // Get the information we need from domain
+    req(domain, function(err, res, html){
+      var $ = cheerio.load(html);
 
-    // Extract website <title> from domain
-      // Save to json api
+      // Scrape elements with text attribute
+      var scrape = function(x, y) {
+        
+        $(x).filter(function(){
+          // Define how to scrape
+          if (y == 'text'){
+            var x = $(this).text();
+          }
+          else if (y == 'attr:content'){
+            var x = $(this).attr('content');
+          }
+          else if (y == 'attr:href'){
+            var x = $(this).attr('href');
+          }
+          else if (y == 'attr:src'){
+            var x = $(this).attr('src');
+          }
+          else if (y == 'attr:alt'){
+            var x = $(this).attr('alt');
+          }
+
+          // Do stuff with the scraped data
+          console.log(x);
+
+        });
+      };
+      
+      // Elements to scrape
+      scrape('title', 'text');
+      scrape('h1', 'text');
+      scrape('meta[name=description]', 'attr:content');
+      scrape('link[rel=icon]', 'attr:href');
+      scrape('img', 'attr:src');
+      scrape('img', 'attr:alt');
+
+    });
 
     // Redirect to /results
     response.render('results', {
       domain: domain
     });
   });
-
-      
-
 };
